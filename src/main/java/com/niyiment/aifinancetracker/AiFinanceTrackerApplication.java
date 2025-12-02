9d880c6ee2ccb4f14231be7368f49eb446777c55
Niyi Ogungbemi <niyiment@gmail.com>
2025-12-02 08:22:49 +0100

    first commit

---
+++ src/main/java/com/niyiment/aifinancetracker/AiFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.niyiment.aifinancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFinanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niyiment/aifinancetracker/AiFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niyiment.aifinancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
